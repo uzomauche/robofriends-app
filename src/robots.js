7b3d4d3c5320c64c929a6bf6698b83cd084102e2
uzoma uche <paradise4uzzy@gmail.com>
2020-05-16 01:39:24 +0100

    first commit

---
+++ src/robots.js
@@ -0,0 +1,64 @@
+export const robots =[
+    {
+        
+        id: 1,
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@april.biz"
+    },
+    {
+        id: 2,
+        name: "Ervin Howell",
+        username: "Antonette",
+        email: "Shanna@melissa.tv"
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net'
+    },
+    {
+        id: 4,
+        name: 'Moonsong Base',
+        username: 'Karianne',
+        email: 'Julianne.0Conner@kory.org'
+    },
+    {
+        id: 5,
+        name: 'The Fel Estate',
+        username: 'Airey',
+        email: 'airey.org'
+    },
+    {
+        id: 6,
+        name: 'Sabre Island',
+        username: 'Cnrianne',
+        email: 'lianne.Conner@yory.org'
+    },
+    {
+        id: 7,
+        name: 'Lifeblood Mansion',
+        username: 'Fence',
+        email: 'Byte.rks@dt.org'
+    },
+    {
+        id: 8,
+        name: 'The Oasis',
+        username: 'aknnie',
+        email: 'quanne@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Whispervale Mansion',
+        username: 'Ynonne',
+        email: 'wuernne.r@wory.org'
+    },
+    {
+        id: 10,
+        name: 'The Epoch',
+        username: 'rianne',
+        email: 'anne.onner@vory.org'
+    },
+    
+]
